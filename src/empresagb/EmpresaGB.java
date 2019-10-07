7396531b6a41de1210d22c30643ae0d17d9267f7
HnryFORD <56278510+HnryFORD@users.noreply.github.com>
2019-10-07 16:04:03 -0600

    un hola mundo

--- src/empresagb/EmpresaGB.java
+++ src/empresagb/EmpresaGB.java
@@ -16,6 +16,8 @@ public class EmpresaGB {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        SystemOutPrinlt("HOLA MUNDO");
+        
     }
     
 }
